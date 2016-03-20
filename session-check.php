4e91f04af21df7f51483cfe1d55d5588a05cd435
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-20 23:07:17 +0530

    Fix bug with reinitiating session

--- session-check.php
+++ session-check.php
@@ -1,16 +1,16 @@
 <?php
-        session_start();
-        if(!isset($_SESSION['username']))
-        {
-            ?>
-                    <script type="text/javascript">
-                        function redirect()
-                            {
-                                window.location="index.php";
-                            }
-                        alert("You have to login to view this page");
-                        setTimeout(redirect(), 1000);
-                    </script>
-            <?php
-
-        }
+if(!isset($_SESSION)) {
+  session_start();
+}
+if(!isset($_SESSION['username'])){
+    ?>
+            <script type="text/javascript">
+                function redirect()
+                    {
+                        window.location="index.php";
+                    }
+                alert("You have to login to view this page");
+                setTimeout(redirect(), 1000);
+            </script>
+    <?php
+}
