5557a01a06f3d7b3237708110b93ca21627e73ca
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-21 00:02:24 +0530

    Fix warning message about new object creation

--- getRollNoDetails.php
+++ getRollNoDetails.php
@@ -3,13 +3,18 @@ require 'connect.php';
 require 'config.php';
 $query="SELECT * FROM `$rollnodetails`";
 $result = $db->query($query) or die ('There was an error during Database Entry [' . $db->error . ']');
-$reply=array();$counter = 0;
+class Student {
+}
+$reply = array();
+$counter = 0;
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        $reply[$counter]->rollno = $row['rollNo'];
-        $reply[$counter]->name = $row['studentName'];
-        $reply[$counter]->department = $row['Department'];
-        $reply[$counter]->gender = $row['Gender'];
+        $student = new Student;
+        $student->rollno = $row['rollNo'];
+        $student->name = $row['studentName'];
+        $student->department = $row['Department'];
+        $student->gender = $row['Gender'];
+        $reply[$counter] = $student;
         $counter++;
     }
 }
