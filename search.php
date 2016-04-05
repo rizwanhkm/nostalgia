414eb1190f98b317f250c930061d11a7b9fb9d6c
rizwanhkm <rizwan.hkm@gmail.com>
2016-04-05 16:42:36 +0530

    Delete unused file

--- search.php
+++
@@ -1,3 +0,0 @@
-    <input type="text" class="text" id="rollnosearch" placeholder="Start typing Roll Number or Name">
-    <div id="searchResultsMsg" class="searchResultsMsg"></div>
-    <table id="searchResults" class="searchResults"></table>
